5a0dcab5eb256882835834c31d77f66595908c88
Jason <jasonscloete@gmail.com>
2019-07-22 21:04:00 +0200

    Added binary counter

--- Prac1/binary_counter.py
+++ Prac1/binary_counter.py
@@ -6,7 +6,7 @@ Readjust this Docstring as follows:
 Names: Jason Cloete
 Student Number: CLTJAS004
 Prac: Practical 1
-Date: <22/07/2019>
+Date: 22/07/2019
 """
 
 # import Relevant Librares
@@ -15,11 +15,12 @@ import time
 
 # Logic that you write
 def main():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(26, GPIO.OUT)
-    GPIO.output(26, GPIO.HIGH)
-    time.sleep(10)
-    GPIO.output(26, GPIO.LOW)
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(21, GPIO.OUT)
+	GPIO.output(21, GPIO.HIGH)
+	time.sleep(10)
+	GPIO.output(21, GPIO.LOW)
+	time.sleep(5)
 
 
 # Only run the functions if 
@@ -32,7 +33,3 @@ if __name__ == "__main__":
         print("Exiting gracefully")
         # Turn off your GPIOs here
         GPIO.cleanup()
-    except e:
-        GPIO.cleanup()
-        print("Some other error occurred")
-        print(e.message)
